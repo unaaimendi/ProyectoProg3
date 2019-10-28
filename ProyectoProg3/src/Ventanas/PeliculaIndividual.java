dff3ad347ab83d836dbee124921a4929e200618e
unaaimendi <48806877+unaaimendi@users.noreply.github.com>
2019-10-28 16:57:58 +0100

    Update PeliculaIndividual.java
    Ventana de PeliculaIndividual

--- ProyectoProg3/src/Ventanas/PeliculaIndividual.java
+++ ProyectoProg3/src/Ventanas/PeliculaIndividual.java
@@ -1,9 +1,29 @@
 package Ventanas;
 
-public class PeliculaIndividual {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class PeliculaIndividual extends JFrame{
 
 	public PeliculaIndividual() {
 		// TODO Auto-generated constructor stub
+		setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
+		setTitle( "Pelicula Individual" ); //Aqui estaria bien poner el nombre de la pelicula mas adelante con un getName()
+		setSize( 800, 600 );
+		setLocationRelativeTo( null ); 
+		
+		JPanel desc = new JPanel();
+		JTextArea textDesc = new JTextArea();
+		desc.add(textDesc);
+		getContentPane().add(desc);
 	}
+
+	public static void main(String[] args) {
+		PeliculaIndividual v = new PeliculaIndividual();
+		v.setVisible( true );
+	}
+
+
 
 }
